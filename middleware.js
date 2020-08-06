8a871f65f8501c698108b281b03e12facefc3032
MukkuMayc <st063775@student.spbu.ru>
2020-08-06 14:20:40 +0300

    Update server

--- middleware.js
+++ middleware.js
@@ -1,25 +1,28 @@
-const jwt = require('jsonwebtoken');
-const secret = 'mysecretsshhh';
+const jwt = require("jsonwebtoken");
+const { set } = require("mongoose");
+const secret = "mysecretsshhh";
 
-const withAuth = function(req, res, next) {
-  const token = 
-      req.body.token ||
-      req.query.token ||
-      req.headers['x-access-token'] ||
-      req.cookies.token;
+const withAuth = function (req, res, next) {
+  const token =
+    req.body.token ||
+    req.query.token ||
+    req.headers["x-access-token"] ||
+    req.cookies.token;
+
+  console.log("token", token);
 
   if (!token) {
-    res.status(401).send('Unauthorized: No token provided');
+    res.status(401).send("Unauthorized: No token provided");
   } else {
-    jwt.verify(token, secret, function(err, decoded) {
+    jwt.verify(token, secret, function (err, decoded) {
       if (err) {
-        res.status(401).send('Unauthorized: Invalid token');
+        res.status(401).send("Unauthorized: Invalid token");
       } else {
         req.email = decoded.email;
         next();
       }
     });
   }
-}
+};
 
 module.exports = withAuth;
--- server.js
+++ server.js
@@ -1,22 +1,29 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const path = require('path');
-const jwt = require('jsonwebtoken');
-const mongoose = require('mongoose');
-const User = require('./models/User');
-const withAuth = require('./middleware');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const path = require("path");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+const User = require("./models/User");
+const withAuth = require("./middleware");
+const cors = require("cors");
 
 const app = express();
 
-const secret = 'mysecretsshhh';
+const secret = "mysecretsshhh";
+
+const corsOptions = {
+  origin: true,
+  credentials: true,
+};
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(cors(corsOptions));
 
-const mongo_uri = 'mongodb://localhost/react-auth';
-mongoose.connect(mongo_uri, { useNewUrlParser: true }, function(err) {
+const mongo_uri = "mongodb://localhost/react-auth";
+mongoose.connect(mongo_uri, { useNewUrlParser: true }, function (err) {
   if (err) {
     throw err;
   } else {
@@ -24,75 +31,161 @@ mongoose.connect(mongo_uri, { useNewUrlParser: true }, function(err) {
   }
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.get('/api/home', function(req, res) {
-  res.send('Welcome!');
+app.get("/api/home", function (req, res) {
+  res.send("Welcome!");
 });
 
-app.get('/api/secret', withAuth, function(req, res) {
-  res.send('The password is potato');
+app.get("/hello", function (req, res) {
+  res.send("Hello!");
 });
 
-app.post('/api/register', function(req, res) {
-  const { email, password } = req.body;
+app.get("/api/secret", withAuth, function (req, res) {
+  res.send("The password is potato");
+});
+
+app.post("/api/register", function (req, res) {
+  const { email, password } = req.body.credentials;
   const user = new User({ email, password });
-  user.save(function(err) {
+  user.save(function (err) {
     if (err) {
       console.log(err);
       res.status(500).send("Error registering new user please try again.");
     } else {
-      res.status(200).send("Welcome to the club!");
+      res.status(200).send("Welcome to the club, buddy!");
     }
   });
 });
 
-app.post('/api/authenticate', function(req, res) {
-  const { email, password } = req.body;
-  User.findOne({ email }, function(err, user) {
+app.post("/login", function (req, res) {
+  console.log(req.body.credentials);
+  const { email, password } = req.body.credentials;
+  User.findOne({ email }, function (err, user) {
     if (err) {
       console.error(err);
-      res.status(500)
-        .json({
-        error: 'Internal error please try again'
+      res.status(500).json({
+        error: "Internal error please try again",
       });
     } else if (!user) {
-      res.status(401)
-        .json({
-        error: 'Incorrect email or password'
+      res.status(401).json({
+        error: "Incorrect email or password",
       });
     } else {
-      user.isCorrectPassword(password, function(err, same) {
+      user.isCorrectPassword(password, function (err, same) {
         if (err) {
-          res.status(500)
-            .json({
-            error: 'Internal error please try again'
+          res.status(500).json({
+            error: "Internal error please try again",
           });
         } else if (!same) {
-          res.status(401)
-            .json({
-            error: 'Incorrect email or password'
+          res.status(401).json({
+            error: "Incorrect email or password",
           });
         } else {
           // Issue token
           const payload = { email };
           const token = jwt.sign(payload, secret, {
-            expiresIn: '1h'
+            expiresIn: "1h",
           });
-          res.cookie('token', token, { httpOnly: true }).sendStatus(200);
+          console.log(token);
+          res.cookie("token", token).sendStatus(200);
         }
       });
     }
   });
 });
 
-app.get('/checkToken', withAuth, function(req, res) {
+app.get("/checkToken", withAuth, function (req, res) {
   res.sendStatus(200);
+});
+
+app.get("/grade", withAuth, (req, res) => {
+  const credential = //req.body.email || req.body.username ||
+        req.query.email || req.query.username;
+  switch (credential) {
+    case "ordinary": {
+      res.status(200).json({
+        courses: [
+          {
+            course_id: "course-v1:spbu+OLYMP+fall_2019",
+            grades: [{ grade_name: "Аудирование", grade: "0.334" }],
+          },
+          {
+            course_id: "course-v1:spbu+OLYMP_retake+fall_2019",
+            grades: [
+              { grade_name: "Чтение", grade: "0.5" },
+              { grade_name: "Лексика.Грамматика", grade: "0.8" },
+            ],
+          },
+        ],
+      });
+      break;
+    }
+    case "a@a.a": {
+      res.status(200).json({
+        courses: [
+          {
+            course_id: "course-v1:spbu+OLYMP+fall_2019",
+            grades: [{ grade_name: "Аудирование", grade: "0.334" }],
+          },
+          {
+            course_id: "course-v1:spbu+OLYMP_retake+fall_2019",
+            grades: [
+              { grade_name: "Чтение", grade: "0.5" },
+              { grade_name: "Лексика.Грамматика", grade: "0.8" },
+            ],
+          },
+        ],
+      });
+      break;
+    }
+    case "nogrades": {
+      res.status(200).json({
+        courses: [
+          {
+            course_id: "course-v1:spbu+OLYMP+fall_2019",
+            // grades: [{ grade_name: "Аудирование", grade: "0.334" }],
+          },
+          {
+            course_id: "course-v1:spbu+OLYMP_retake+fall_2019",
+            // grades: [
+            //   { grade_name: "Чтение", grade: "0.5" },
+            //   { grade_name: "Лексика.Грамматика", grade: "0.8" },
+            // ],
+          },
+        ],
+      });
+      break;
+    }
+    case "nocourses": {
+      res.status(200).json({
+        courses: [],
+        //   courses: [
+        //     {
+        //       course_id: "course-v1:spbu+OLYMP+fall_2019",
+        //       // grades: [{ grade_name: "Аудирование", grade: "0.334" }],
+        //     },
+        //     {
+        //       course_id: "course-v1:spbu+OLYMP_retake+fall_2019",
+        //       // grades: [
+        //       //   { grade_name: "Чтение", grade: "0.5" },
+        //       //   { grade_name: "Лексика.Грамматика", grade: "0.8" },
+        //       // ],
+        //     },
+        //   ],
+        // };
+      });
+      break;
+    }
+
+    default: {
+      res.status(404).send("There is no such user");
+    }
+  }
 });
 
 app.listen(process.env.PORT || 8080);
