719fd2df3d0b579b7be06b791cf313b06000de3c
MukkuMayc <st063775@student.spbu.ru>
2020-08-07 03:43:13 +0300

    Add endpoint for proctoring

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.post("/login", function (req, res) {
           // Issue token
           const payload = { email };
           const token = jwt.sign(payload, secret, {
-            expiresIn: "1h",
+            expiresIn: "10h",
           });
           console.log(token);
           res.cookie("token", token).sendStatus(200);
@@ -104,8 +104,7 @@ app.get("/checkToken", withAuth, function (req, res) {
 });
 
 app.get("/grade", withAuth, (req, res) => {
-  const credential = //req.body.email || req.body.username ||
-        req.query.email || req.query.username;
+  const credential = req.query.email || req.query.username;
   switch (credential) {
     case "ordinary": {
       res.status(200).json({
@@ -148,14 +147,9 @@ app.get("/grade", withAuth, (req, res) => {
         courses: [
           {
             course_id: "course-v1:spbu+OLYMP+fall_2019",
-            // grades: [{ grade_name: "Аудирование", grade: "0.334" }],
           },
           {
             course_id: "course-v1:spbu+OLYMP_retake+fall_2019",
-            // grades: [
-            //   { grade_name: "Чтение", grade: "0.5" },
-            //   { grade_name: "Лексика.Грамматика", grade: "0.8" },
-            // ],
           },
         ],
       });
@@ -164,20 +158,94 @@ app.get("/grade", withAuth, (req, res) => {
     case "nocourses": {
       res.status(200).json({
         courses: [],
-        //   courses: [
-        //     {
-        //       course_id: "course-v1:spbu+OLYMP+fall_2019",
-        //       // grades: [{ grade_name: "Аудирование", grade: "0.334" }],
-        //     },
-        //     {
-        //       course_id: "course-v1:spbu+OLYMP_retake+fall_2019",
-        //       // grades: [
-        //       //   { grade_name: "Чтение", grade: "0.5" },
-        //       //   { grade_name: "Лексика.Грамматика", grade: "0.8" },
-        //       // ],
-        //     },
-        //   ],
-        // };
+      });
+      break;
+    }
+
+    default: {
+      res.status(404).send("There is no such user");
+    }
+  }
+});
+
+app.get("/proctoring", withAuth, (req, res) => {
+  const credential = req.query.email || req.query.username; //req.body.email || req.body.username ||
+  switch (credential) {
+    case "ordinary": {
+      res.status(200).json({
+        courses: [
+          {
+            course_id: "course-v1:spbu+OLYMP+fall_2019",
+            links: [{ test_name: "Аудирование", url: "https://test.com/1" }],
+          },
+          {
+            course_id: "course-v1:spbu+OLYMP_retake+fall_2019",
+            links: [
+              { test_name: "Чтение", url: "https://test.com/2" },
+              { test_name: "Лексика.Грамматика", url: "https://test.com/3" },
+            ],
+          },
+        ],
+      });
+      break;
+    }
+    case "a@a.a": {
+      res.status(200).json({
+        courses: [
+          {
+            course_id: "course-v1:spbu+OLYMP+fall_2019",
+            links: [{ test_name: "Аудирование", url: "https://test.com/1" }],
+          },
+          {
+            course_id: "course-v1:spbu+OLYMP_retake+fall_2019",
+            links: [
+              { test_name: "Чтение", url: "https://test.com/2" },
+              { test_name: "Лексика.Грамматика", url: "https://test.com/3" },
+            ],
+          },
+        ],
+      });
+      break;
+    }
+    case "nogrades": {
+      res.status(200).json({
+        courses: [
+          {
+            course_id: "course-v1:spbu+OLYMP+fall_2019",
+          },
+          {
+            course_id: "course-v1:spbu+OLYMP_retake+fall_2019",
+          },
+        ],
+      });
+      break;
+    }
+    case "nocourses": {
+      res.status(200).json({
+        courses: [],
+      });
+      break;
+    }
+
+    case "longlinks": {
+      res.status(200).json({
+        courses: [
+          {
+            course_id: "course-v1:spbu+OLYMP_retake+fall_2019",
+            links: [
+              {
+                test_name: "Чтение",
+                url:
+                  "https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.ytimg.com%2Fvi%2FWl959QnD3lM%2Fmaxresdefault.jpg&imgrefurl=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DWl959QnD3lM&tbnid=YsuHKK6-7EWmGM&vet=12ahUKEwiY3LHoyofrAhVXxioKHTE7BQwQMygAegUIARClAQ..i&docid=ntkyOwngEaK1oM&w=1280&h=720&client=firefox-b-d&ved=2ahUKEwiY3LHoyofrAhVXxioKHTE7BQwQMygAegUIARClAQ",
+              },
+              {
+                test_name: "Лексика.Грамматика",
+                url:
+                  "https://www.google.com/imgres?imgurl=https%3A%2F%2Fi.ytimg.com%2Fvi%2FlaaUFxhzqIs%2Fmaxresdefault.jpg&imgrefurl=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DlaaUFxhzqIs&tbnid=T7a1m6XxAUxfkM&vet=12ahUKEwiY3LHoyofrAhVXxioKHTE7BQwQMyg9egQIARA7..i&docid=3cht7D2YcQ_9sM&w=1280&h=720&client=firefox-b-d&ved=2ahUKEwiY3LHoyofrAhVXxioKHTE7BQwQMyg9egQIARA7",
+              },
+            ],
+          },
+        ],
       });
       break;
     }
